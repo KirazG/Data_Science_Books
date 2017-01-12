53cb5da611da777e9a72b34bf51875a153203455
Miraj <miraj.vashi@gmail.com>
2017-01-12 18:53:52 +0530

    DS_BOOKS_05

---
+++ Machine Learning/Introduction_To_Statistical_Learning/Solutions/CH_02_Statistical_Learning/CH02_R_Exercise.R
@@ -0,0 +1,10 @@
+#################################################################################
+##### BOOK: INTRODUCTION TO STATISTICAL LEARNING                            #####
+##### CHAPTER: 02 - STATISTICAL LEARNING                                    #####
+#################################################################################
+
+##### Cleanup environment
+rm(list = ls(all.names = TRUE))
+require(ISLR)
+require(MASS)
+
